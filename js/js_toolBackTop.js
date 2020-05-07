2dfd4f1125da35a291cd402ee94a8c2a09ba7fbf
caozhengbiao <caozhengbiao@foxmail.com>
2020-05-07 12:20:04 +0800

    Add files via upload

---
+++ js/js_toolBackTop.js
@@ -0,0 +1,20 @@
+/*toolBackTop.js*/
+$(document).ready(function () {
+        var bt = $('#toolBackTop');
+        var sw = $(document.body)[0].clientWidth;
+
+        var limitsw = (sw - 1060) / 2 - 40;
+        if (limitsw > 0){
+                limitsw = parseInt(limitsw);
+                bt.css("right",limitsw);
+        }
+
+        $(window).scroll(function() {
+                var st = $(window).scrollTop();
+                if(st > 30){
+                        bt.show();
+                }else{
+                        bt.hide();
+                }
+        });
+})
